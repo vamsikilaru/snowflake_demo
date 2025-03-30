ed2bc1c1b0617889b8a64d9422a0efe4e8164da4
Vamsi Kilaru <vamsi.techdeveloper@gmail.com>
2025-03-30 11:06:16 -0700

    Update streamlit_app.py
    removed errors

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-#from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
 
 helpful_links = [
     "https://docs.streamlit.io",
@@ -13,12 +13,12 @@ helpful_links = [
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write("Choose the fruits you want in your custom Smoothie!")
 
-from snowflake.snowpark.functions import col
+
 
 name_on_order = st.text_input("Name on Smoothie")
 st.write("The name on your smoothie will be:", name_on_order)
 
-cnx = st.connection('snowflake')
+cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
