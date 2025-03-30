4fafca61d12c58dd94887c92edd868915f49f218
Vamsi Kilaru <vamsi.techdeveloper@gmail.com>
2025-03-30 10:52:21 -0700

    changed SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session
 
 helpful_links = [
     "https://docs.streamlit.io",
@@ -18,7 +18,8 @@ from snowflake.snowpark.functions import col
 name_on_order = st.text_input("Name on Smoothie")
 st.write("The name on your smoothie will be:", name_on_order)
 
-session = get_active_session()
+cnx = st.connection('snowflake')
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_list = st.multiselect("""
